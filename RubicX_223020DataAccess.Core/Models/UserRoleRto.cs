46f4324cea7aa7486e2b91ed27fb3dd35249533a
StudentDev <90097489+Ler4i@users.noreply.github.com>
2021-11-14 17:54:58 +0300

    Сделали сервер и немного БД

---
+++ RubicX_223020DataAccess.Core/Models/UserRoleRto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RubicX_223020DataAccess.Core.Models
+{
+    [Table("UserRole")]
+    public class UserRoleRto
+    {
+        [Key] public int Id { get; set; }//столбец
+        public string Name { get; set; }
+
+    }
+}
---
+++ RubicX_223020DataAccess.Core/Models/UserRto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RubicX_223020DataAccess.Core.Models
+{
+    [Table("User")]
+    public class UserRto
+    {
+        [Key]  public int Id { get; set; }//столбец
+        public string Email { get; set; }
+        public string Login { get; set; }
+        [Required] public string PhoneNumderPrefix { get; set; }
+        [Required] public string PhoneNumder { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Patronymic { get; set; }
+        public DateTimeOffset Birthday { get; set; }
+        public string AvatarUrl { get; set; }
+        public UserRoleRto Role { get; set; }
+        
+    }
+}
